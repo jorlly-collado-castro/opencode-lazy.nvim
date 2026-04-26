dae61f2d7437b649a0fc863d9bc6b958e8e7f7d7
jorlly-collado-castro <263438472+jorlly-collado-castro@users.noreply.github.com>
2026-04-26 16:59:30 +0200

    feat: extend keybindings and add which-key integration

--- lua/opencode-lazy/init.lua
+++ lua/opencode-lazy/init.lua
@@ -1,8 +1,28 @@
 -- lua/opencode-lazy/init.lua
 return {
 	"NickvanDyke/opencode.nvim",
+	version = "*",
 	dependencies = {
-		{ "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+		---@module "snacks"
+		{
+			"folke/snacks.nvim",
+			optional = true,
+			opts = {
+				input = {}, -- Enhances `ask()`.
+				picker = {}, -- Enhances `select()`.
+				terminal = {}, -- Enables the `snacks` provider.
+			},
+		},
+		---@module "which-key"
+		{
+			"folke/which-key.nvim",
+			opts = {
+				spec = {
+					{ "<leader>a", mode = { "n", "x" }, group = "OpenCode" },
+					{ "<leader>ap", mode = { "n", "x" }, group = "Prompt" },
+				},
+			},
+		},
 	},
 	keys = {
 		-- INFO: Open OpenCode in a split view. Requires two key presses.
@@ -62,7 +82,7 @@ return {
 			desc = "Page Up (OpenCode)",
 			mode = "n",
 		},
-		-- INFO: Scrolls the OpenCode session up by half a page.
+		-- INFO: Scrolls the OpenCode session down by half a page.
 		{
 			"<S-C-d>",
 			function()
@@ -70,6 +90,111 @@ return {
 			end,
 			desc = "Page Down (OpenCode)",
 			mode = "n",
+		},
+
+		------------------------------------------------------------------------------------------------------
+		-- INFO: Ask a question.
+		{
+			"<leader>ai",
+			function()
+				require("opencode").ask("", { submit = true })
+			end,
+			mode = { "n", "x" },
+			desc = "Ask",
+		},
+		-- INFO: Ask a question about the current context.
+		{
+			"<leader>aI",
+			function()
+				require("opencode").ask("@this: ", { submit = true })
+			end,
+			mode = { "n", "x" },
+			desc = "Ask with context",
+		},
+		-- INFO: Ask a question about the current buffer.
+		{
+			"<leader>ab",
+			function()
+				require("opencode").ask("@file: ", { submit = true })
+			end,
+			mode = { "n", "x" },
+			desc = "Ask about buffer",
+		},
+		------------------------------------------------------------------------------------------------------
+		-- INFO: Prompt OpenCode to explain the code.
+		{
+			"<leader>ape",
+			function()
+				require("opencode").prompt("explain", { submit = true })
+			end,
+			mode = { "n", "x" },
+			desc = "Explain",
+		},
+		-- INFO: Prompt OpenCode to fix the code.
+		{
+			"<leader>apf",
+			function()
+				require("opencode").prompt("fix", { submit = true })
+			end,
+			mode = { "n", "x" },
+			desc = "Fix",
+		},
+		-- INFO: Prompt OpenCode to diagnose the code.
+		{
+			"<leader>apd",
+			function()
+				require("opencode").prompt("diagnose", { submit = true })
+			end,
+			mode = { "n", "x" },
+			desc = "Diagnose",
+		},
+		-- INFO: Prompt OpenCode to review the code.
+		{
+			"<leader>apr",
+			function()
+				require("opencode").prompt("review", { submit = true })
+			end,
+			mode = { "n", "x" },
+			desc = "Review",
+		},
+		-- INFO: Prompt OpenCode to generate tests.
+		{
+			"<leader>apt",
+			function()
+				require("opencode").prompt("test", { submit = true })
+			end,
+			mode = { "n", "x" },
+			desc = "Test",
+		},
+		-- INFO: Prompt OpenCode to optimize the code.
+		{
+			"<leader>apo",
+			function()
+				require("opencode").prompt("optimize", { submit = true })
+			end,
+			mode = { "n", "x" },
+			desc = "Optimize",
+		},
+		------------------------------------------------------------------------------------------------------
+		-- INFO: Add a range to OpenCode.
+		{
+			"go",
+			function()
+				return require("opencode").operator("@this ")
+			end,
+			expr = true,
+			mode = { "n", "x" },
+			desc = "Add range to OpenCode",
+		},
+		-- INFO: Add a line to OpenCode.
+		{
+			"goo",
+			function()
+				return require("opencode").operator("@this ") .. "_"
+			end,
+			expr = true,
+			mode = { "n" },
+			desc = "Add line to OpenCode",
 		},
 	},
 	config = function()
@@ -93,5 +218,53 @@ return {
 
 		-- Required for `opts.events.reload`.
 		vim.o.autoread = true
+
+		vim.api.nvim_create_autocmd({ "TermOpen" }, {
+			group = vim.api.nvim_create_augroup("opencode_buffer", { clear = false }),
+			pattern = "*:opencode --port*",
+			desc = "Assign binds specific to OpenCode buffers",
+			callback = function(event)
+				local bufname = vim.api.nvim_buf_get_name(event.buf) ---@type string
+
+				-- Example: `term://.../nvim/lua/plugins//58118:opencode --port`
+				-- The dashes in `--port` must be escaped because it is a special
+				-- character in Lua's flavour of regex patterns.
+				if string.match(bufname, "^term:.+//%d+:opencode %-%-port.*$") then
+					require("which-key").add({
+						buffer = event.buf,
+						mode = { "n", "t" },
+						{
+							"<C-U>",
+							function()
+								require("opencode").command("session.half.page.up")
+							end,
+							desc = "Half scroll backward",
+						},
+						{
+							"<C-D>",
+							function()
+								require("opencode").command("session.half.page.down")
+							end,
+							desc = "Half scroll forward",
+						},
+						{
+							"<C-B>",
+							function()
+								require("opencode").command("session.page.up")
+							end,
+							desc = "Scroll backward",
+						},
+						{
+							"<C-F>",
+							function()
+								require("opencode").command("session.page.down")
+							end,
+							desc = "Scroll forward",
+						},
+					})
+				end
+			end,
+		})
 	end,
 }
+
